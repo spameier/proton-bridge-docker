37effb135b69635c54e7adfa71ac2a55a8bea64a
spameier <jonas@rabe.ch>
2020-09-11 15:30:37 +0200

    migrate to alpine based image

--- Dockerfile
+++ Dockerfile
@@ -1,39 +1,16 @@
-FROM debian:buster-slim
+FROM alpine AS build
+RUN ["apk","add","--no-cache","gcc","git","go","libsecret-dev","make","pkgconf"]
+WORKDIR /go/src/github.com/ProtonMail/proton-bridge
+RUN ["wget","https://github.com/ProtonMail/proton-bridge/archive/master.tar.gz"]
+RUN ["tar","xf","master.tar.gz"]
+WORKDIR proton-bridge-master
+RUN ["sed","-i","s/REVISION:=.*/REVISION:=git/","Makefile"]
+RUN ["make","clean"]
+RUN ["make","build-nogui"]
 
-MAINTAINER Sébastien Delafond <sdelafond@gmail.com>
-
-ARG PROTONMAIL_BRIDGE_VERSION
-ARG SMTP_PORT
-ARG IMAP_PORT
-
-ENV PROTONMAIL_BRIDGE_FILE=protonmail-bridge_${PROTONMAIL_BRIDGE_VERSION}_amd64.deb
-ENV USER=protonmail
-ENV DEBIAN_FRONTEND=noninteractive
-
-# install packages
-COPY ./${PROTONMAIL_BRIDGE_FILE} .
-RUN apt-get update -q && \
-    apt-get install --no-install-recommends --no-install-suggests --yes \
-        ./${PROTONMAIL_BRIDGE_FILE} \
-	ca-certificates \
- 	pass \
- 	socat \
- 	libcap2-bin && \
-    apt-get clean && \
-    rm ${PROTONMAIL_BRIDGE_FILE}
-
-RUN setcap 'cap_net_bind_service=+ep' /usr/bin/socat
-
-# copy entrypoint and related files to dedicated user's home directory
-RUN useradd -m -s /bin/bash ${USER}
-COPY entrypoint.sh gpg-key-parameters.txt /home/${USER}/
-RUN chown -R ${USER}: /home/${USER}
-
-# document which ports are exposed
-EXPOSE ${SMTP_PORT}
-EXPOSE ${IMAP_PORT}
-
-# user, workdir, entrypoint
-USER ${USER}
-WORKDIR /home/${USER}
-ENTRYPOINT ./entrypoint.sh
+FROM alpine
+LABEL maintainer="spameier"
+RUN ["apk","add","dbus","glib","libsecret","pass","socat"]
+COPY --from=build /go/src/github.com/ProtonMail/proton-bridge/proton-bridge-master/Desktop-Bridge /
+ADD entrypoint.sh /
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,48 @@
-#!/bin/bash
+#!/bin/sh
+# heaviliy inspired by https://github.com/sdelafond/docker-protonmail-bridge
 
-set -e
+set -eux
 
 # constants
-BRIDGE="protonmail-bridge --cli"
-BRIDGE_IMAP_PORT="1143"
-BRIDGE_SMTP_PORT="1025"
-GPG_PARAMS="${HOME}/gpg-key-parameters.txt"
-FIFO="/tmp/fifo"
-# other variables are coming from the environment itself
+BRIDGE="/Desktop-Bridge --cli --log-level info"
+BRIDGE_IMAP_PORT=1143
+BRIDGE_SMTP_PORT=1025
+FIFO="/fifo"
 
-# main
-if ! [ -d ~/.gnupg ] ; then
-  gpg --generate-key --batch ${GPG_PARAMS}
+# check if required variables are set
+# FIXME make more compact
+die() { echo "$1"; exit 1; }
+if [ -z "$PM_USER" ]; then
+  die '$PM_USER is not set'
+elif [ -z "$PM_PASS" ]; then
+  die '$PM_PASS is not set'
+elif [ -z "$IMAP_PORT" ]; then
+  die '$IMAP_PORT is not set'
+elif [ -z "$SMTP_PORT" ]; then
+  die '$SMTP_PORT is not set'
 fi
 
-if ! [ -d ~/.password-store ] ; then
-  pass init "$(awk -F: '/^Name-Real/ {print $2}' ${GPG_PARAMS})"
+# initialize gpg if necessary
+if ! [ -d /root/.gnupg ]; then
+  gpg --generate-key --batch << 'EOF'
+    %no-protection
+    %echo Generating GPG key
+    Key-Type:RSA
+    Key-Length:4096
+    Name-Real:proton-bridge
+    Expire-Date:0
+    %commit
+EOF
 fi
 
-if ! [ -f ~/.cache/protonmail/bridge ] ; then
-  echo -e "login\n${PROTONMAIL_LOGIN}\n${PROTONMAIL_PASSWORD}\n${PROTONMAIL_EXTRA_2FA}" | ${BRIDGE} ${BRIDGE_EXTRA_ARGS}
+# initialize pass if necessary
+if ! [ -d /root/.password-store ]; then
+  pass init proton-bridge
+fi
+
+# login to ProtonMail if neccessary
+if ! [ -f /root/.cache/protonmail/bridge ]; then
+  printf "login\n%s\n%s\n" "${PM_USER}" "${PM_PASS}" | ${BRIDGE}
 fi
 
 # socat will make the connection appear to come from 127.0.0.1, since
@@ -29,5 +51,10 @@ socat TCP-LISTEN:${SMTP_PORT},fork TCP:127.0.0.1:${BRIDGE_SMTP_PORT} &
 socat TCP-LISTEN:${IMAP_PORT},fork TCP:127.0.0.1:${BRIDGE_IMAP_PORT} &
 
 # display account information, then keep stdin open
-[ -e ${FIFO} ] || mkfifo ${FIFO}
-{ echo info ; cat ${FIFO} ; } | ${BRIDGE} ${BRIDGE_EXTRA_ARGS}
+if ! [ -e ${FIFO} ]; then
+  mkfifo ${FIFO}
+fi
+{
+  printf "info\n";
+  cat ${FIFO}
+} | ${BRIDGE}
